dea32728fd961f3daeec4946c398775ab736a6c0
bip-zip <bipindhakal44@gmail.com>
2023-05-29 12:42:16 +0545

    fix reminder

--- reminder/views.py
+++ reminder/views.py
@@ -6,7 +6,15 @@ def home(request):
     form = TaskReminderForm
     today = datetime.today()
     today_reminder = TaskReminder.objects.filter(timedate__year=today.year, timedate__month=today.month, timedate__day=today.day).order_by('priority')
-    context={'form':form,'today_reminder':today_reminder}
+    future_reminder = TaskReminder.objects.filter(timedate__year__gte=today.year, timedate__month__gte=today.month, timedate__day__gte=today.day).order_by('priority')
+    past_reminder = TaskReminder.objects.filter(timedate__year__lte=today.year, timedate__month__lte=today.month, timedate__day__lte=today.day).order_by('priority')
+    context={
+        'form':form,
+        'today_reminder':today_reminder,
+        'future_reminder':future_reminder,
+        'past_reminder':past_reminder
+
+        }
     return render(request,'reminder-home.html', context)
 
 def formSubmit(request):
